69f5b3342d0c96d15733f507f413370e90aaf389
Sebas Echazú <sebastianechazu@outlook.com>
2025-04-26 09:56:22 -0300

    Refactor header component and remove demo page
    - Updated HeaderFixed component to include a white background and z-index for better visibility.
    - Deleted the demo.astro page as it was no longer needed.
    - Modified global CSS to import design tokens.
    - Removed unused tailwind.css file.
    - Cleaned up tsconfig.json by removing unnecessary comments.
    - Created a new index.astro page to serve as the main entry point, incorporating various components and sections.

--- config.ts
+++ config.ts
@@ -1,33 +1,33 @@
-export interface SiteConfig {
-    author: string;
-    authorUrl: string;
-    site: string;
-    siteLegend: string;
-    title: string;
-    description: string;
-    url: string;
-    favicon: string;
-    brandSrc: string;
-    brandAlt: string;
-    mailto: string;
-    social: {
-        instagram?: string;
-    }
-}
+// export interface SiteConfig {
+//     author: string;
+//     authorUrl: string;
+//     site: string;
+//     siteLegend: string;
+//     title: string;
+//     description: string;
+//     url: string;
+//     favicon: string;
+//     brandSrc: string;
+//     brandAlt: string;
+//     mailto: string;
+//     social: {
+//         instagram?: string;
+//     }
+// }
 
-export const config: SiteConfig = {
-    author: 'Default Agency',
-    authorUrl: 'https://wwww.defaultftn.com',
-    site: 'Default Name',
-    siteLegend: 'Default legend',
-    title: 'Default title',
-    description: 'Default description',
-    url: 'https://wwww.defaultnamecompany.com/',
-    favicon: '/default.svg',
-    brandSrc: '/defaultBrand.svg',
-    brandAlt: 'Default Brand ALT',
-    mailto: 'mailto:default@company.com',
-    social: {
-        instagram: 'default'
-    },
-}
+// export const config: SiteConfig = {
+//     author: 'Default Agency',
+//     authorUrl: 'https://wwww.defaultftn.com',
+//     site: 'Default Name',
+//     siteLegend: 'Default legend',
+//     title: 'Default title',
+//     description: 'Default description',
+//     url: 'https://wwww.defaultnamecompany.com/',
+//     favicon: '/default.svg',
+//     brandSrc: '/defaultBrand.svg',
+//     brandAlt: 'Default Brand ALT',
+//     mailto: 'mailto:default@company.com',
+//     social: {
+//         instagram: 'default'
+//     },
+// }
