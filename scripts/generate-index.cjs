0c3738fc29e71dacd8745199741ec3716bd41de1
Sebas Echazú <sebastianechazu@outlook.com>
2025-04-28 16:52:08 -0300

    refactor: Update package.json exports and enhance generate-index script to create index.d.ts

--- scripts/generate-index.cjs
+++ scripts/generate-index.cjs
@@ -2,37 +2,48 @@ const fs = require('fs');
 const path = require('path');
 
 const componentsDir = path.resolve(__dirname, '../dist/components');
-const outputFile = path.resolve(__dirname, '../dist/index.ts');
+const outputFileTs = path.resolve(__dirname, '../dist/index.ts');
+const outputFileDts = path.resolve(__dirname, '../dist/index.d.ts');
 
 // Función para obtener los nombres de los componentes
 function getComponentExports(dir, basePath = './components') {
     let exports = [];
+    let types = [];
 
     const files = fs.readdirSync(dir);
     for (const file of files) {
         const fullPath = path.join(dir, file);
         const stat = fs.statSync(fullPath);
 
-        // Si es un directorio, llama recursivamente
         if (stat.isDirectory()) {
-            exports = exports.concat(getComponentExports(fullPath, `${basePath}/${file}`));
+            const { exports: childExports, types: childTypes } = getComponentExports(fullPath, `${basePath}/${file}`);
+            exports = exports.concat(childExports);
+            types = types.concat(childTypes);
         } else if (file.endsWith('.astro')) {
-            // Obtener el nombre del archivo sin la extensión
             const componentName = path.basename(file, '.astro');
             exports.push(`export { default as ${componentName} } from '${basePath}/${file}';`);
+            types.push(`export const ${componentName}: AstroComponentFactory;`);
         }
     }
 
-    return exports;
+    return { exports, types };
 }
 
-// Función para generar el archivo index.ts
+// Función para generar el archivo index.ts e index.d.ts
 function generateIndex() {
-    const exports = getComponentExports(componentsDir);
-    const content = exports.join('\n') + '\n';
+    const { exports, types } = getComponentExports(componentsDir);
 
-    fs.writeFileSync(outputFile, content);
+    const tsContent = exports.join('\n') + '\n';
+    fs.writeFileSync(outputFileTs, tsContent);
     console.log('✅ index.ts generado con éxito.');
+
+    const dtsContent = `declare module '@forever-twenty-nine/astro-ui-lib' {
+  import type { AstroComponentFactory } from 'astro';
+
+${types.join('\n')}
+}\n`;
+    fs.writeFileSync(outputFileDts, dtsContent);
+    console.log('✅ index.d.ts generado con éxito.');
 }
 
 generateIndex();
